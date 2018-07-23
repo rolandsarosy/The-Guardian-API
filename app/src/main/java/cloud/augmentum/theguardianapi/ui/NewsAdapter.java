e7d3dfebe6a134b86d9b0fec723b06287e4c0bfc
Roland Sárosy <36110029+rolandsarosy@users.noreply.github.com>
2018-07-23 09:47:43 +0200

    Clean up the CardView

--- app/src/main/java/cloud/augmentum/theguardianapi/ui/NewsAdapter.java
+++ app/src/main/java/cloud/augmentum/theguardianapi/ui/NewsAdapter.java
@@ -20,6 +20,7 @@ public class NewsAdapter extends ArrayAdapter<Result> {
     private List<Result> newsList;
     private boolean isExtended = false;
 
+    // Public constructor for the adapter
     public NewsAdapter(Context context, List<Result> list) {
         super(context, 0, list);
         mContext = context;
@@ -29,30 +30,37 @@ public class NewsAdapter extends ArrayAdapter<Result> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View listItem = convertView;
-
         if (listItem == null) {
             listItem = LayoutInflater.from(mContext).inflate(R.layout.list_item, parent, false);
         }
-
+        
+        // Set the current news item in the list
         Result currentNews = newsList.get(position);
 
+        // Find reference to the views in the list item
         TextView name = listItem.findViewById(R.id.article_name);
         TextView category = listItem.findViewById(R.id.article_category);
         final TextView body = listItem.findViewById(R.id.article_body);
         Button learnMoreButton = listItem.findViewById(R.id.article_button);
 
+        // Parse the body text of the current news item
         List<Body> currentNewsBodyElements = currentNews.getBlocks().getBody();
         Body currentNewsBody = currentNewsBodyElements.get(0);
+        // The full body text will be used when the user has expanded the CardView
         final String bodyTextFull = currentNewsBody.getBodyTextSummary();
+        // The summary of the body text will be used when the CardView is retracted
         final String bodyText = bodyTextFull.substring(0, Math.min(bodyTextFull.length(), CatalogActivity.BODY_MAX_TEXT_LENGHT)).concat("...");
 
+        // Parse the name and category for the current news item
         String nameString = currentNews.getWebTitle();
         String categoryString = currentNews.getSectionName();
 
+        // Set the parsed values
         name.setText(nameString);
         category.setText(categoryString);
         body.setText(bodyText);
 
+        // onClickListener for the button to extend and retract the CardView 
         learnMoreButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
